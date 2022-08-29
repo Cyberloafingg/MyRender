d38dfce2fc2cf30471e4b42c6c1f6590ce603531
cyberloafing <934058879@qq.com>
2022-08-29 17:14:53 +0800

    L2

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,6 +7,7 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model* model = NULL;
 const int width = 800;
 const int height = 800;
@@ -42,6 +43,86 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
     }
 }
 
+//void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+//    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+//    if (t0.y > t1.y) std::swap(t0, t1);
+//    if (t0.y > t2.y) std::swap(t0, t2);
+//    if (t1.y > t2.y) std::swap(t1, t2);
+//    int total_height = t2.y - t0.y;
+//    for (int y = t0.y; y <= t1.y; y++) {
+//        int segment_height = t1.y - t0.y + 1;
+//        float alpha = (float)(y - t0.y) / total_height;
+//        float beta = (float)(y - t0.y) / segment_height; // be careful with divisions by zero 
+//        Vec2i A = t0 + (t2 - t0) * alpha;
+//        Vec2i B = t0 + (t1 - t0) * beta;
+//        if (A.x > B.x) std::swap(A, B);
+//        for (int j = A.x; j <= B.x; j++) {
+//            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+//        }
+//    }
+//    for (int y = t1.y; y <= t2.y; y++) {
+//        int segment_height = t2.y - t1.y + 1;
+//        float alpha = (float)(y - t0.y) / total_height;
+//        float beta = (float)(y - t1.y) / segment_height; // be careful with divisions by zero 
+//        Vec2i A = t0 + (t2 - t0) * alpha;
+//        Vec2i B = t1 + (t2 - t1) * beta;
+//        if (A.x > B.x) std::swap(A, B);
+//        for (int j = A.x; j <= B.x; j++) {
+//            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+//        }
+//    }
+//    //for (int i = 0; i < total_height; i++) {
+//    //    bool half = i > t1.y - t0.y || t1.y == t0.y;
+//    //    int segment_height = half ? t2.y - t1.y : t1.y - t0.y;
+//    //    float alpha = (float)i / total_height;
+//    //    float beta = (float)(i - (half ? t1.y - t0.y : 0)) / segment_height;
+//    //    Vec2i A = t0 + (t2 - t0) * alpha;
+//    //    Vec2i B = half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+//    //    if (A.x > B.x) std::swap(A, B);
+//    //    for (int j = A.x; j <= B.x; j++) {
+//    //        image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y 
+//    //    }
+//    //}
+//
+//}
+
+
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+    Vec2i t[3] = { t0, t1, t2 };
+    Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+    Vec2i bboxmax(0, 0);
+    for (int i = 0; i < 3; i++) {
+        bboxmin.x = std::max(0, std::min(bboxmin.x, t[i].x));
+        bboxmin.y = std::max(0, std::min(bboxmin.y, t[i].y));
+
+        bboxmax.x = std::min(image.get_width() - 1, std::max(bboxmax.x, t[i].x));
+        bboxmax.y = std::min(image.get_height() - 1, std::max(bboxmax.y, t[i].y));
+    }
+    // 遍历box区域，判断像素是否在三角内
+    Vec2i P,AP,BP,CP;
+    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+            AP = P - t0; BP = P - t1; CP = P - t2;
+            Vec2i arr[3] = {AP, BP, CP};
+            if( ((arr[0].x * arr[1].y - arr[0].y * arr[1].x)*(arr[1].x * arr[2].y - arr[1].y * arr[2].x)) >= 0 && 
+                ((arr[2].x * arr[0].y - arr[2].y * arr[0].x)*(arr[0].x * arr[1].y - arr[0].y * arr[1].x)) >= 0){
+                image.set(P.x, P.y, color);
+            }
+            //int ts = 1;
+            //for (int i = 0; i < 3; i++) {
+            //    ts *= (arr[i].x * arr[(i + i) % 3].y - arr[i].y * arr[(i + 1) % 3].x);
+            //}
+            //if (ts > 0) {
+            //    image.set(P.x, P.y, color);
+            //}
+        }
+    }
+}
+
+
+
+
 int main(int argc, char** argv) {
     if (2 == argc) {
         model = new Model(argv[1]);
@@ -49,22 +130,17 @@ int main(int argc, char** argv) {
     else {
         model = new Model("F:/CGraphic/TinyRender/TinyRender/Project1/obj/african_head.obj");
     }
-
+    // 绘制模型
     TGAImage image(width, height, TGAImage::RGB);
-    // 读取model，遍历faces的面数
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
         for (int j = 0; j < 3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            int x0 = (v0.x + 1.) * width / 2.;
-            int y0 = (v0.y + 1.) * height / 2.;
-            int x1 = (v1.x + 1.) * width / 2.;
-            int y1 = (v1.y + 1.) * height / 2.;
-            line(x0, y0, x1, y1, image, white);
+            Vec3f world_coords = model->vert(face[j]);
+            screen_coords[j] = Vec2i((world_coords.x + 1.) * width / 2., (world_coords.y + 1.) * height / 2.);
         }
+        triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
     }
-
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
--- Project1/model.cpp
+++ Project1/model.cpp
@@ -17,7 +17,7 @@ Model::Model(const char* filename) : verts_(), faces_() {
         if (!line.compare(0, 2, "v ")) {
             iss >> trash;
             Vec3f v;
-            for (int i = 0; i < 3; i++) iss >> v.raw[i];
+            for (int i = 0; i < 3; i++) iss >> v[i];
             verts_.push_back(v);
         }
         else if (!line.compare(0, 2, "f ")) {
@@ -46,11 +46,9 @@ int Model::nfaces() {
 }
 
 std::vector<int> Model::face(int idx) {
-    // face[idx1, idx2, idx3]
     return faces_[idx];
 }
 
 Vec3f Model::vert(int i) {
-    // vert[x, y, z]
     return verts_[i];
 }
